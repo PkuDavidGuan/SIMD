6813e0754f5781bcb032b16732597fc12ea4d9d2
Yushuo Guan <1400012923@pku.edu.cn>
2016-12-19 18:56:00 +0800

    initial yuv

---
+++ yuv.cpp
@@ -0,0 +1,48 @@
+/*
+本模板仅供参考
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+
+	return 0;
+}
+
+
+
+//下面的4个函数应该统计出图像处理的时间;
+//函数参数和返回值可以需要自己定.
+int process_without_simd(){
+	int time = 0;
+
+	/*处理过程*/
+
+	return time;
+}
+
+int process_with_mmx(){
+	int time = 0;
+
+	/*处理过程*/
+
+	return time;
+}
+
+int process_with_sse(){
+	int time = 0;
+
+	/*处理过程*/
+
+	return time;
+}
+
+int process_with_avx(){
+	int time = 0;
+	/*处理过程*/
+
+
+	return time;
+}
